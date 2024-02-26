3a483537b37884ded3bea8af971b6d18e2409f7a
sinh <sinhdangvan27@gmail.com>
2024-02-26 19:11:49 +0700

    Laravel-Bai10

--- laravel/app/Http/Controllers/CategoriesController.php
+++ laravel/app/Http/Controllers/CategoriesController.php
@@ -2,83 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+
+use function PHPSTORM_META\type;
 
 class CategoriesController extends Controller
 {
     //
-    public function __construct(Request $request)
-    {
-        // echo 'Welcome';
-        /*
-        Nếu là trang danh sách chuyên mục => hiển thị ra dòng chữ: xin chào Unicode
-         */
-        // if ($request-> is('categories')){
-        //     echo '<h3>xin chào Unicode</h3>';
-        // }
-        }
-    // Hiển thị danh sách chuyên mục (Phương thức GET)
+    public function __construct(){
+        echo 'welcome ff';
+    }
+    //hien thi  list chuyen muc - get
     public function index(Request $request){
-        // if(isset($_GET['id'])){
-        //     echo $_GET['id'];
-        // }
+        // $allData = $request->all();
+        // dd($allData);
+        // $url =$request->fullUrl();
+        // echo $url;
+        // $ip =$request->ip();
+        // echo 'ip la'.$ip;
+        // $method =$request->method();
+        // echo $method;
+        // $id = $request->query('id');
+        // dd($id);
+        $query = $request->query();
+        dd($query);
+        return view('clients/categories/list');  
+    }
+    //Lays ra mot chuyen muc theo id - get
+    public function getCategory($id){
+        return view('clients/categories/edit');  
+    }
+    //Cap nhat mot nchuyen muc-post
+    public function updateCategory($id){
+        return 'submit category:'.$id;
+    }
+    //show  form them du lieu -GET
+    public function addCategory(Request $request){
         // $path = $request->path();
         // echo $path;
-        // $url = $request->url();
-        // $fulUrl = $request->fullUrl();
-        // echo $fulUrl;
-        // $method = $request->method();
-        // echo $method;
-        $ip = $request->ip();
-        // echo 'IP là: '.$ip;
-        // if($request->isMethod('GET')){
-        //     echo 'Phương thức get';
-        // }
-        // $server = $request->serve();
-        // print_r($server);
-        // dd($_SERVER);
-        // $header = $request ->header('user-agent');
-        // dd($header);
-        // $id = $request ->input('id');
-        // echo $id;
-        // $id = $request ->input('id.*.name');
-        // $id = $request->id;
-        // $name =$request->name;
-        // dd($id);
-        $name = request('name', 'Unicode');
-        dd($name);
-        return view("clients/categories/list");
+        $cateName = $request->old('category');
+        echo $cateName;
+        return view('clients/categories/add');  
     }
-    // Lấy ra một chuyên mục theo id (Phương thức GET)
-    public function getCategory($id){
-        return view("clients/categories/edit");
-    }
-    // Sửa 1 chuyên mục (Phương thức POST)
-    public function updateCategory($id){
-        return "Submit sửa chuyên mục: ".$id;
-    }
-    // Show form thêm dữ liệu (Phương thức GET)
-    public function addCategory(Request $request){
-        $path = $request->path();
-        echo $path;
-        return view("clients/categories/add");
-    }
-    // Thêm dữ liệu vào chuyên mục (Phương thức POST)
-    public function handleAddCategory(Request $request){
-        // $allData = $request->all();
-        // echo $request->all()['name'];
-        // dd($allData);
-     
-
-        // if($request->isMethod('POST')){
-        //     echo 'Phương thức post ';
-        // }
+    //them du lieu vao chuyen muc- POst
+    public function handleAddCategory(Request $request ){
+        // $cateName = $request->query();
+        if($request->has('category_name')){
+            $cateName = $request->category_name;
+            // dd($cateName);
+            $request->flash();
+            return redirect(route('categories.add'));
+        }else{
+            return 'kh coa category';
+        }
+        
         // return redirect(route('categories.add'));
-        // return "Submit thêm chuyên mục";
+        // return 'submit them category:';
     }
-    // Xóa dữ liệu (Phương thức delete)
+    //xoa du lieu - delete
     public function deleteCategory($id){
-        return "Submit xóa chuyên mục".$id;
+        return 'submit xoas category:'.$id;
+    }
+    public function getFile(){
+    return view('clients/categories/file');
+    }
+    //xu ly lay thong tuin file
+    public function handleFile(Request $request){
+        if($request->hasFile('photo')){
+            if($request->photo->isValid()){
+                $file = $request->photo;
+                $path = $file->path();
+                $ext = $file->extension();
+                dd($path);
+            }
+            // $file = $request->file('photo');
+            // dd($file);
+        }else{
+            return 'vui long chon file';
+        }
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -70,22 +70,24 @@ Route::prefix('admin')->group(function () {
 
 Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth.admin');
 // Cleint router :: 
-Route::prefix('category')->group(function() {
-  // danh sách chuyên mục
-    Route::get('/',[CategoriesController::class, 'index'])->name('categories.list');
-
-  Route::get('/edit/{id}', [CategoriesController::class,'getCategory'])->name('categories.edit');
-
-  Route::post('/edit/{id}', [CategoriesController::class, 'updateCategory'])->name('categories');
-
-  //Hiển thị form add dữ liệu 
-  Route::get('/add', [CategoriesController::class, 'addCategory'])->name('categories.add');
-
-  Route::post('/add', [CategoriesController::class, 'handleAddcategory']);
-
-  // Xóa chuyên mục
-  Route::delete('/delete/{id}', [CategoriesController::class, 'deleteCategory']);
-
+Route::get('/home',[homeController::class,'index'])->name('home');
+Route::prefix('categories')->group(function () {
+    //List of categories
+    Route::get('/', [CategoriesController::class, 'index'])->name('categories.list');
+    //Get details of a category (Apply show form to edit categories)
+    Route::get('/edit/{id}', [CategoriesController::class, 'getCategory'])->name('categories.edit');
+    //Handle updates chuyen muc
+    Route::post('/edit/{id}', [CategoriesController::class, 'updateCategory']);
+    //hien thi form add data
+    Route::get('/add', [CategoriesController::class, 'addCategory'])->name('categories.add');
+    //xu ly them chuyen muc/ handle add categorry
+    Route::post('/add', [CategoriesController::class, 'handleAddCategory']);
+    // delete category
+    Route::delete('/delete/{id}', [CategoriesController::class, 'deleteCategory'])->name('categories.delete');
+    // hien thi form upload
+    Route::get('/upload', [CategoriesController::class, 'getFile']);
+    //xu li file
+    Route::post('/upload', [CategoriesController::class, 'handleFile'])->name('categories.upload');
 });
 
 Route::prefix('admin')->group(function(){

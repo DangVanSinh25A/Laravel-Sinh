8cd60ece311432372f4b41f020a07035a2a64e9a
sinh <sinhdangvan27@gmail.com>
2024-02-29 21:27:32 +0700

    Laravel-Bai12

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,45 +1,100 @@
-<h1>Trang chủ Unicode </h1>
-<h2>{{$welcome}}</h2>
-<h2>{{!empty(request()->keyword)?request()->keyword:"khoong có gì"}}</h2>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unicode</title>
+</head>
+
+<body>
+    <header>
+        <h1>Unicode</h1>
+    </header>
+    <main>
+        <h1>Nội dung Unicode</h1>
+        <h2><?php echo $content ?></h2>
+        <h1>Trang chủ unicode</h1>
+        <!-- Cbhuyển tất cả mã html sang dạng thực thể: giúp an toàn hơn -->
+        <h2>{{$welcome}}</h2>
+        <!-- Toán tử ba ngôi và cần biên dịch html -->
+        <!-- <h2>{{!empty(request() -> keyword)?request()->keyword:'Không có gì'}}</h2>
 <div class="container">
-    {!!$content!!};
+    {!!$content!!}
 </div>
-<!-- @for($i =1; $i<=10; $i++)
-<p>Phần tử thứ {{$i}}</p>
-@endfor -->
-<!-- @while($index<=10)
-<p>Phần tử thứ {{$index}}</p>
-@php $index++;;
- @endphp
-@endwhile -->
-<!-- @foreach($dataArr as $key =>$item)
-<p>Phần tử: {{$item}} -{{$key}}</p>
-@endforeach; -->
-<!-- @forelse($dataArr as $item)
-<p>Phần tư: {{$item}}</p>
-@empty
-<p>Không có phần tử này</p>
-@endforelse; -->
 
-<!-- @if($number>10)
-<p>Đây là giá trị hợp lê</p>
-@endif -->
- <!-- @if($number<0)
-<p>Số âm</p>
-@elseif($number>=0  && $number<5)
-<p>đây là giá trị bằng 5</p>
-@else
-<p>Đây là giá trị nhỏ hơn 10</p>
-@endif;  -->
+@for($i =1;$i <=10;$i++) <p>Phần thử thứ: {{$i}}</p>
+    @endfor
 
-@switch($number)
-@case(1)
-<p>Số thứ nhất</p>
-@break
-@case(2)
-<p>Số còn lại</p>
-@break
-@default
-<p>Số còn lại</p>
-@endswitch
+    @while($index <= 10) <p>Phần thử thứ: {{$index}}</p>
+        @php
+            $index++;
+        @endphp
+    @endwhile -->
+
+        @foreach ($dataArr as $key =>$item)
+        <p>Phần tử: {{$item}} - {{$key}}</p>
+        @endforeach
+
+        @forelse ($dataArr as $item)
+        <p>Phần tử: {{$item}}</p>
+        @empty
+        <p>Không có phần tử nào</p>
+        @endforelse
+
+        @if ($number >=10)
+        <p>Đây là giá trị hợp lệ</p>
+        @elseif ($number <0) <p>SỐ âm</p>
+            @else
+            <p>Giá trị không hợp lẹ</p>
+            @endif
+
+            @switch($number)
+            @case(1)
+            @case(4)
+            @case(7)
+            <p>Số thứ nhất</p>
+            @break
+            @case(2)
+            <p>SỐ thứ hai</p>
+            @break
+            @default
+            <p>số còn lại</p>
+            @endswitch
+
+            @php
+            $numeber =10;
+            if($number >= 10){
+            $total += 10;
+            }else
+            $total = $number +20;
+            @endphp
+            <h3>kết quả {{$number}} - total: {{$total}}</h3>
+
+            <!-- @forelse ($dataArr as $item)
+            <p>Phần tử: {{$item}}</p>
+            @empty
+            <p>Không có phần tử nào</p>
+            @endforelse -->
+            <!-- Chỉ comment html chứ lệnh vẫn chạy -->
+
+
+            {{-- @forelse ($dataArr as $item)
+            <p>Phần tử: {{$item}}</p>
+            @empty
+            <p>Không có phần tử nào</p>
+            @endforelse -->
+            <!-- Chỉ comment html chứ lệnh vẫn chạy --}}
+            <!-- Comment dành cho templte -->
+
+            {{--@php
+            $message ='Đặt hàng thành công';
+            @endphp--}}
+            @include('parts.notice')
+    </main>
+    <footer>
+        <h1>Footer</h1>
+    </footer>
+</body>
+
+</html>

ed5e465e6301d650afd4878958810f3366f56e00
sinh <sinhdangvan27@gmail.com>
2024-03-18 22:33:20 +0700

    Laravel-Bai31

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,32 +2,44 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\UsersController;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Users extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
-    //Khai bảo một thuộc tính table
-    protected $table = 'users';
-    public function getAllUsers()
-    {
-        $users = DB::select('SELECT * FROM users ORDER BY created_at DESC');
-        return $users;
-    }
-    public function addUser($data)
-    {
-        DB::insert('INSERT INTO users (name, email, created_at) VALUE (?, ? , ?)', $data);
-    }
-    public function getDetail($id)
-    {
-        return DB::select('SELECT* FROM ' . $this->table . ' WHERE id = ? ', [$id]);
-    }
-    public function updateUser($data, $id)
-    {
-        $data[] = $id;
-        return DB::update('UPDATE ' . $this->table . ' SET name = ?, email = ?, updated_at = ? WHERE id = ? ', $data);
-    }
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
